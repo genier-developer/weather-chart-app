ab212b3a4c73d04863f9551815186d5de0cee97a
Olga Genier <genier.developer@gmail.com>
2025-02-28 11:49:37 +0500

    fix: type WeatherChartProps

--- src/components/weather-chart/weather-chart.tsx
+++ src/components/weather-chart/weather-chart.tsx
@@ -7,14 +7,28 @@ type WeatherChartProps = {
     main: {
       temp: number;
     }
-  }
+  }[];
 }
+
+const normalizeTime = (tickItem: string)=>{
+  const date = new Date(tickItem);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  if(hours===0 && minutes===0){
+    return "date+1"
+  }
+  return `${hours.toString().padStart(2, '0')}`;
+}
+
 export const WeatherChart: FC<WeatherChartProps> = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={400}>
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="dt_txt" />
+        <XAxis
+          dataKey="dt_txt"
+          tickFormatter={normalizeTime}
+        />
         <YAxis />
         <Tooltip />
         <Legend />
