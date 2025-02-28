aadb077328f4dc52d90fd1798500065ec3d59c5b
Olga Genier <genier.developer@gmail.com>
2025-02-28 11:56:50 +0500

    fix: display time on Axis X

--- src/components/weather-chart/weather-chart.tsx
+++ src/components/weather-chart/weather-chart.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import './weather-chart.scss';
 
 type WeatherChartProps = {
   data: {
@@ -15,25 +16,27 @@ const normalizeTime = (tickItem: string)=>{
   const hours = date.getHours();
   const minutes = date.getMinutes();
   if(hours===0 && minutes===0){
-    return "date+1"
+    return `${date.toLocaleDateString('en-US', {month: 'short', day: 'numeric'})}`;
   }
   return `${hours.toString().padStart(2, '0')}`;
 }
 
 export const WeatherChart: FC<WeatherChartProps> = ({ data }) => {
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="dt_txt"
-          tickFormatter={normalizeTime}
-        />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="main.temp" stroke="#8884d8" />
-      </LineChart>
-    </ResponsiveContainer>
+    <div className="weather-chart-container">
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="dt_txt"
+            tickFormatter={normalizeTime}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="main.temp" stroke="#1976d2" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
