9192248fa08167730b247a0171ab63ba5a2999c7
Olga Genier <genier.developer@gmail.com>
2025-02-28 11:22:55 +0500

    feat: add chart

--- src/components/weather-chart/weather-chart.tsx
+++ src/components/weather-chart/weather-chart.tsx
@@ -1,9 +1,25 @@
 import {FC} from "react";
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 
-export const WeatherChart: FC = () => {
+type WeatherChartProps = {
+  data: {
+    dt_txt: string;
+    main: {
+      temp: number;
+    }
+  }
+}
+export const WeatherChart: FC<WeatherChartProps> = ({ data }) => {
   return (
-    <div>
-      Weather Chart
-    </div>
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="dt_txt" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="main.temp" stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
